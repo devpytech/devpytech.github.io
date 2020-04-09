6ffd85924794b85cc8045ba4f094d54a9364b45a
Robert Washbourne <rawashbourne@gmail.com>
2020-04-09 15:55:22 -0500

    remake

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -1,61 +1,71 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const cacheName = 'blogCache';
+const offlineUrl = '/offline/';
+const adminPageSlug = '/ghost';
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  '../index.html',
-  'css/normalize.css',
-  'css/tachyons.min.css',
-  'css/screen.css',
-  'css/prism-tomorrow.min.css',
-  'prism.min.js',
-  'prism-bash.min.js'
-];
-
-// The install handler takes care of precaching the resources we always need.
+/**
+ * The event listener for the service worker installation
+ */
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches.open(cacheName)
+    .then(cache => cache.addAll([
+      offlineUrl
+    ]))
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
+/**
+ * Is the current request for an HTML page?
+ * @param {Object} event
+ */
+function isHtmlPage(event) {
+  return event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html');
+}
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
+/**
+ * Is the current request for the admin portal resource
+ * @param {Object} event
+ */
+function isAdminPageResource(event) {
+  return event.request.url.includes(adminPageSlug);
+}
+
+/**
+ * Fetch and cache any results as we receive them.
+ */
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      // Only return cache if it's not an HTML page or admin resources
+      if (response && !isHtmlPage(event) && !isAdminPageResource(event)) {
+        return response;
+      }
+
+      return fetch(event.request).then(
+        function (response) {
+          // Dont cache if not a 200 response
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
+          let responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function (cache) {
+            cache.put(event.request, responseToCache);
           });
-        });
-      })
-    );
-  }
+
+          return response;
+        }
+      ).catch(error => {
+        // Check if the user is offline first and is trying to navigate to a web page
+        if (isHtmlPage(event)
+        ) {
+          return caches.match(offlineUrl);
+        }
+      });
+    })
+  )
+  ;
 });
